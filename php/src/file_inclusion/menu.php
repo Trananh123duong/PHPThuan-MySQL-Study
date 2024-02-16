8ccab1d3a96846860356eec377857ddba3c3bc9c
dev05 <dev05@dev.co-mit.com>
2024-02-16 09:09:12 +0700

    file inclusion

---
+++ php/src/file_inclusion/menu.php
@@ -0,0 +1,4 @@
+<a href="http://hoclaptrinh.vn/index.jsp">Home</a> - 
+<a href="http://hoclaptrinh.vn/php">PHP</a> - 
+<a href="http://hoclaptrinh.vn/java">JAVA</a> - 
+<a href="http://hoclaptrinh.vn/html">HTML</a> <br />
